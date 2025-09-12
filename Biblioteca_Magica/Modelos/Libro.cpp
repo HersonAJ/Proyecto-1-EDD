34d73bf509e0cb1df2c0108cdda9fcb021cff566
HersonAJ <hersonaguilar202230685@cunoc.edu.gt>
2025-09-11 18:28:23 -0600

    modelo de libro

---
+++ Biblioteca_Magica/Modelos/Libro.cpp
@@ -0,0 +1,65 @@
+#include "Libro.h"
+#include <string>
+#include  <sstream>
+
+//constructor con parametros
+Libro::Libro(const std::string &titulo,
+    const std::string &isbn,
+    const std::string &genero,
+    const std::string &fecha,
+    const std::string &autor)
+    : titulo(titulo), isbn(isbn), genero(genero), fecha(fecha), autor(autor) {}
+
+//getters
+std::string Libro::getTitulo() const {
+    return titulo;
+}
+
+std::string Libro::getIsbn() const {
+    return isbn;
+}
+std::string Libro::getGenero() const {
+    return genero;
+}
+std::string Libro::getFecha() const {
+    return fecha;
+}
+std::string Libro::getAutor() const {
+    return autor;
+}
+
+//setters
+void Libro::setTitulo(const std::string& titulo) {
+    this->titulo = titulo;
+}
+void Libro::setIsbn(const std::string& isbn) {
+    this->isbn = isbn;
+}
+void Libro::setGenero(const std::string& genero) {
+    this->genero = genero;
+}
+void Libro::setFecha(const std::string& fecha) {
+    this->fecha = fecha;
+}
+void Libro::setAutor(const std::string& autor) {
+    this->autor = autor;
+}
+
+std::string Libro::toString() const {
+    std::ostringstream oss;
+    oss << "Titulo: " << (titulo.empty() ? "<vacío>" : titulo)
+        << " | ISBN: " << (isbn.empty() ? "<vacío>" : isbn)
+        << " | Genero: " << (genero.empty() ? "<vacío>" : genero)
+        << " | Fecha: " << (fecha.empty() ? "<vacío>" : fecha)
+        << " | Autor: " << (autor.empty() ? "<vacío>" : autor);
+    return oss.str();
+}
+
+
+int Libro::compararPorTitulo(const Libro& otro) const {
+    return  titulo.compare(otro.titulo);
+}
+
+int Libro::compararPorIsbn(const Libro& otro) const {
+    return isbn.compare(otro.isbn);
+}
---
+++ Biblioteca_Magica/Modelos/Libro.h
@@ -0,0 +1,39 @@
+#ifndef BIBLIOTECA_MAGICA_LIBRO_H
+#define BIBLIOTECA_MAGICA_LIBRO_H
+
+#include  <string>
+
+class Libro {
+private:
+    std::string titulo;
+    std::string isbn;
+    std::string genero;
+    std::string fecha;
+    std::string autor;
+
+public:
+    Libro();
+    Libro(const std::string& titulo, const std::string& isbn, const std::string& genero, const std::string& fecha, const std::string& autor);
+
+    //getters
+    std::string getTitulo() const;
+    std::string getIsbn() const;
+    std::string getGenero() const;
+    std::string getFecha() const;
+    std::string getAutor() const;
+
+    //setters
+    void setTitulo(const std::string& titulo);
+    void setIsbn(const std::string& isbn);
+    void setGenero(const std::string& genero);
+    void setFecha(const std::string& fecha);
+    void setAutor(const std::string& autor);
+
+    std::string toString() const;
+
+    //comparaciones para organizar los nodos
+    int compararPorTitulo(const Libro& libro) const;
+    int compararPorIsbn(const Libro& libro) const;
+};
+
+#endif //BIBLIOTECA_MAGICA_LIBRO_H
